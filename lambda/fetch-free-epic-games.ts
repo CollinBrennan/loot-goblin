5347cce57ddb2ac296d03e6bfa74624d17d490a4
collin <collinbrennandev@gmail.com>
2025-07-05 14:52:05 -0400

    Only post offers not in the db

--- lambda/fetch-free-epic-games.ts
+++ lambda/fetch-free-epic-games.ts
@@ -3,6 +3,7 @@ import { DISCORD_MAX_EMBED_LENGTH } from '../constants';
 import { putOffer } from '../db/offer';
 import createGameEmbeds from '../embeds/game-embed';
 import { epic } from '../services';
+import type { Game } from '../types';
 
 export const handler = async () => {
   const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
@@ -10,19 +11,29 @@ export const handler = async () => {
   if (!webhookUrl) throw new Error('Discord webhook URL not defined');
 
   const games = await fetchFreeEpicGames();
-  const embeds = createGameEmbeds(games, epic).slice(
-    0,
-    DISCORD_MAX_EMBED_LENGTH,
-  );
 
-  games.forEach(({ offerId, endDate }) => {
-    const ttl = Math.floor(endDate.getTime() / 1000);
-    putOffer({ offerId, service: epic.name, ttl });
-  });
+  const gamesToPost: Game[] = [];
 
-  await fetch(webhookUrl, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ embeds }),
-  });
+  for (const game of games) {
+    const ttl = Math.floor(game.endDate.getTime() / 1000);
+
+    try {
+      await putOffer({ offerId: game.offerId, service: epic.name, ttl });
+      gamesToPost.push(game);
+    } catch {
+      continue;
+    }
+  }
+
+  if (gamesToPost.length > 0) {
+    const embeds = createGameEmbeds(gamesToPost, epic).slice(
+      0,
+      DISCORD_MAX_EMBED_LENGTH,
+    );
+    await fetch(webhookUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ embeds }),
+    });
+  }
 };
--- lambda/fetch-free-prime-games.ts
+++ lambda/fetch-free-prime-games.ts
@@ -1,8 +1,10 @@
+import { fetchFreeEpicGames } from '../api/epic';
 import { fetchFreePrimeGames } from '../api/prime';
 import { DISCORD_MAX_EMBED_LENGTH } from '../constants';
 import { putOffer } from '../db/offer';
 import createGameEmbeds from '../embeds/game-embed';
 import { prime } from '../services';
+import type { Game } from '../types';
 
 export const handler = async () => {
   const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
@@ -10,19 +12,29 @@ export const handler = async () => {
   if (!webhookUrl) throw new Error('Discord webhook URL not defined');
 
   const games = await fetchFreePrimeGames();
-  const embeds = createGameEmbeds(games, prime).slice(
-    0,
-    DISCORD_MAX_EMBED_LENGTH,
-  );
 
-  games.forEach(({ offerId, endDate }) => {
-    const ttl = Math.floor(endDate.getTime() / 1000);
-    putOffer({ offerId, service: prime.name, ttl });
-  });
+  const gamesToPost: Game[] = [];
 
-  await fetch(webhookUrl, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ embeds }),
-  });
+  for (const game of games) {
+    const ttl = Math.floor(game.endDate.getTime() / 1000);
+
+    try {
+      await putOffer({ offerId: game.offerId, service: prime.name, ttl });
+      gamesToPost.push(game);
+    } catch {
+      continue;
+    }
+  }
+
+  if (gamesToPost.length > 0) {
+    const embeds = createGameEmbeds(gamesToPost, prime).slice(
+      0,
+      DISCORD_MAX_EMBED_LENGTH,
+    );
+    await fetch(webhookUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ embeds }),
+    });
+  }
 };
